4dd543a8a7e29c0d9a00c0075d0853f57a33e7f5
JerryCheng <jerrycheng96@live.com>
2016-09-17 23:07:08 +0800

    September 17th, 2016

--- Digital_Image_Processing/main.cpp
+++ Digital_Image_Processing/main.cpp
@@ -2,13 +2,13 @@
 
 void main()
 {
-	BMPFile * bmpBW = readBMP("Lenna_BW.bmp");
-	BMPFile * bmpGS = readBMP("applepickinggs.bmp");
-	BMPFile * bmpTC = readBMP("Lenna.bmp");
+	//BMPFile * bmpBW = readBMP("Lenna_BW.bmp");
+	BMPFile * bmpGS = readBMP("Lenna_8-bit.bmp");
+	//BMPFile * bmpTC = readBMP("Lenna.bmp");
 	
 	//writeBMP(bmpBW, "testwrite.bmp");
 	//writeBMP(bmpGS, "testwritegs.bmp");
 	//writeBMP(bmpTC, "testwritetc.bmp");
-	writeBMP(GSToBinByBit(bmpGS, 2), "t.bmp");
+	writeBMP(GSToBin(bmpGS, 135), "t.bmp");
 
 }
